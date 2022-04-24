b0a919ade2db738f7c6f8413044f24e3d55f6034
n-tv <tri.vh15@gmail.com>
2022-04-25 00:23:34 +0700

    Update sb-admin-2.js

--- assets/js/sb-admin-2.js
+++ assets/js/sb-admin-2.js
@@ -37,7 +37,7 @@
   // Scroll to top button appear
   $(document).on('scroll', function() {
     var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
+    if (scrollDistance > 500) {
       $('.scroll-to-top').fadeIn();
     } else {
       $('.scroll-to-top').fadeOut();
