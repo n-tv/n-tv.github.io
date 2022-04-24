871fade1674d03882fb7fa996b2e839fd3085ed7
n-tv <tri.vh15@gmail.com>
2022-04-25 00:41:36 +0700

    Update tooltip.js

--- assets/js/tooltip.js
+++ assets/js/tooltip.js
@@ -1,3 +1,21 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+const goTopBtn = document.querySelector('.scroll-to-top');
+
+window.addEventListener('scroll', checkHeight)
+
+function checkHeight(){
+  if(window.scrollY > 200) {
+    goTopBtn.style.display = "flex"
+  } else {
+    goTopBtn.style.display = "none"
+  }
+}
+
+goTopBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+})
